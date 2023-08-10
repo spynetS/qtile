68039b95f07bd2ff5e2294e27e4c21cecfa2127f
Alfred Roos <alfred@stensatter.se>
2023-08-10 22:52:09 +0200

    bar update

--- config.py
+++ config.py
@@ -158,15 +158,19 @@ screens = [
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
+                widget.PulseVolume(),
+                widget.Battery(),
+                widget.CPU(),
+                widget.Memory(),
+                widget.TextBox("Net:"),
+                widget.NetGraph(),
                 widget.Systray(),
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
                 widget.QuickExit(),
             ],
-            24,
+            32,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
